11d2241f8b3b57cf6c872f5072e0007bef5764f2
achraf9494 <achraf4khemakhem@gmail.com>
2021-04-01 23:17:12 +0200

    checkptstate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state= {Person:{ FullName:"Achraf Khemakhem" , Bio:"Ingenieur" , ImgSrc: <img src="logo192.png" alt="logo"/>,Profession:"Full stack JS"
+
+  },Show: false}
+  toggle =() => this.setState({Show: !this.state.Show})
+  render() {
+    return (
+      <div>
+        
+
+
+        <button onClick={this.toggle}>{this.state.Show ? "Hide" : "Show"}</button> <br/>
+        {
+          this.state.Show ? <div>
+            <h1>Name : {this.state.Person.FullName}</h1> <br/>
+        <h1>Bio : {this.state.Person.Bio}</h1> <br/>
+        <h1>Image : {this.state.Person.ImgSrc}</h1> <br/>
+        <h1>Profession : {this.state.Person.Profession}</h1>
+          </div> : "Click to show your profil"
+        }
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
